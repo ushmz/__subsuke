315d5b8df5b91de691cd1857b1c7bfa61f9d30aa
Yusuke Shimizu <yusuk@cabim60031.local>
2020-03-03 18:51:03 +0900

    設定画面を編集

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import SectionList from 'react-native';
+import React, { Component } from 'react';
+import {SectionList, StyleSheet, Text, View} from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
 
 export default function SettingsScreen() {
@@ -7,9 +7,182 @@ export default function SettingsScreen() {
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+
+  console.log(typeof(<ConfigView />));
+  return <ConfigView />;
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  title: 'Setting',
 };
+
+class ConfigView extends React.Component {
+  render() {
+
+    const sections = [
+      { data: [{ value: 'beta' }], title: 'version' },
+      { data: [{ value: 'Yusuke Shimizu' }], title: 'Auther' },
+      {
+        data: [{ value: '@rabhare6it' }],
+        title: 'Github',
+      },
+      {
+        data: [{ value: 'subsuke.app@gmail.com' }],
+        title: 'Contact',
+      },
+    ];
+
+    return (
+      <SectionList
+        style={styles.container}
+        renderItem={this._renderItem}
+        renderSectionHeader={this._renderSectionHeader}
+        stickySectionHeadersEnabled={true}
+        keyExtractor={(item, index) => index}
+        //ListHeaderComponent={ListHeader}
+        sections={sections}
+      />
+    );
+  }
+
+  _renderSectionHeader = ({ section }) => {
+    return <SectionHeader title={section.title} />;
+  };
+  
+  _renderItem = ({ item }) => {
+    if (item.type === 'color') {
+      return <SectionContent>{item.value && <Color value={item.value} />}</SectionContent>;
+    } else {
+      return (
+        <SectionContent>
+          <Text style={styles.sectionContentText}>{item.value}</Text>
+        </SectionContent>
+      );
+    }
+  };
+
+};
+
+const ListHeader = () => {
+  const { manifest } = Constants;
+
+  return (
+    <View style={styles.titleContainer}>
+      <View style={styles.titleIconContainer}>
+        <AppIconPreview iconUrl={manifest.iconUrl} />
+      </View>
+
+      <View style={styles.titleTextContainer}>
+        <Text style={styles.nameText} numberOfLines={1}>
+          {manifest.name}
+        </Text>
+
+        <Text style={styles.slugText} numberOfLines={1}>
+          {manifest.slug}
+        </Text>
+
+        <Text style={styles.descriptionText}>{manifest.description}</Text>
+      </View>
+    </View>
+  );
+};
+
+const SectionHeader = ({ title }) => {
+  return (
+    <View style={styles.sectionHeaderContainer}>
+      <Text style={styles.sectionHeaderText}>{title}</Text>
+    </View>
+  );
+};
+
+const SectionContent = props => {
+  return <View style={styles.sectionContentContainer}>{props.children}</View>;
+};
+
+const AppIconPreview = ({ iconUrl }) => {
+  if (!iconUrl) {
+    iconUrl = 'https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png';
+  }
+
+  return <Image source={{ uri: iconUrl }} style={{ width: 64, height: 64 }} resizeMode="cover" />;
+};
+
+const Color = ({ value }) => {
+  if (!value) {
+    return <View />;
+  } else {
+    return (
+      <View style={styles.colorContainer}>
+        <View style={[styles.colorPreview, { backgroundColor: value }]} />
+        <View style={styles.colorTextContainer}>
+          <Text style={styles.sectionContentText}>{value}</Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: 'row',
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2,
+  },
+  sectionHeaderContainer: {
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ededed',
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+  },
+  sectionContentContainer: {
+    paddingTop: 8,
+    paddingBottom: 12,
+    paddingHorizontal: 15,
+  },
+  sectionContentText: {
+    color: '#808080',
+    fontSize: 14,
+  },
+  nameText: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  slugText: {
+    color: '#a39f9f',
+    fontSize: 14,
+    backgroundColor: 'transparent',
+  },
+  descriptionText: {
+    fontSize: 14,
+    marginTop: 6,
+    color: '#4d4d4d',
+  },
+  colorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  colorPreview: {
+    width: 17,
+    height: 17,
+    borderRadius: 2,
+    marginRight: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+  },
+  colorTextContainer: {
+    flex: 1,
+  },
+});
